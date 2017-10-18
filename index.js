ea4428f30cceb71cae174200f7b70b3ba4f725d8
chance <zhoucx@dtdream.com>
2017-10-18 21:09:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,14 @@ if(fs.existsSync(path.join(__dirname, 'config.js'))) {
   password = require('./config').password;
 }
 
-var PORT = 29231;
+var PORT = 3000;
 var PONG_DELTA = 10E3;
 var NOT_FOUNT_MSG = '小胡子哥提醒您：404 了！';
 var FAVICON = fs.readFileSync(path.join(__dirname, 'favicon.ico'));
 var whiteList = [
   '0.0.0.0',
   '127.0.0.1',
-  'barret',
-  'localhost',
-  '119.23.13.9',
-  'barretlee.com',
-  'www.barretlee.com',
-  'barret.cc',
-  'www.barret.cc',
-  'xiaohuzige.com',
-  'www.xiaohuzige.com',
+  'chat--room.leanapp.cn',
   'www.cnblogs.com',
   'hustksyking.cnblogs.com'
 ];
